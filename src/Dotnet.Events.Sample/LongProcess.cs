f03361bacbaccfbca10287aaf4d50e57b7679951
Alex Galante <Alex.Galante@grupoboticario.com.br>
2020-03-16 18:36:00 -0300

    *

---
+++ src/Dotnet.Events.Sample/LongProcess.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dotnet.Events.Sample
+{
+    public class LongProcess
+    {
+        public delegate int LongEvent();
+
+        public event LongEvent BeforeEvent;
+
+        public event LongEvent AfterEvent;
+
+        public void Run()
+        {
+            var x = BeforeEvent?.Invoke();
+
+            Console.WriteLine($"Trigger Run Slepping {x} miliseconds");
+            Thread.Sleep(x ?? 1000);
+
+            AfterEvent?.Invoke();
+        }
+    }
+}
---
+++ src/Dotnet.Events.Sample/Program.cs
@@ -0,0 +1,29 @@
+﻿
+    using System;
+using System.Threading;
+
+namespace Dotnet.Events.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var longProcess = new LongProcess();
+            longProcess.BeforeEvent += () =>
+            {
+                Console.WriteLine("Trigger Before Event");
+
+                return new Random().Next(1000, 9999);
+            };
+
+            longProcess.AfterEvent += () =>
+            {
+                Console.WriteLine("Trigger After Event");
+
+                return default;
+            };
+
+            longProcess.Run();
+        }
+    }
+}
